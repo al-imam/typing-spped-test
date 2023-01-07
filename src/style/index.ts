5a03ba68ce91660b6d33d8ac0be0836b754374f6
al-imam <alimam01828@gmail.com>
2023-01-07 21:49:52 +0600

    change style on Title and Container
      - change padding and background-color on Container
      - change font-family on Title

--- src/style/index.ts
+++ src/style/index.ts
@@ -3,12 +3,15 @@ import styled from "styled-components";
 export const Container = styled.div`
   width: min(calc(100vw - 3rem), 76rem);
   margin-inline: auto;
-  border-bottom: 5px solid rgba(20 20 20 / 0.1);
+  padding-inline: 2rem;
+  background-color: aliceblue;
 `;
 
 export const Title = styled.h1`
   font-size: 2.5rem;
   color: #000000;
+  font-family: var(--ff-text);
   padding-top: 1.5rem;
   padding-bottom: 0.5rem;
+  border-bottom: 5px solid rgba(20 20 20 / 0.1);
 `;
