f298dcb712b16aec718b7bb7a565ef5d9a6f3b44
al-imam <alimam01828@gmail.com>
2023-01-07 22:30:50 +0600

    create C component only for memo purpose

--- src/App.tsx
+++ src/App.tsx
@@ -9,3 +9,21 @@ function App() {
 }
 
 export default App;
+
+const C = memo(
+  ({ input, index, text }: { input: string; index: number; text: string }) => (
+    <Character
+      className={
+        input.length === index
+          ? "active"
+          : text === input[index]
+          ? "correct"
+          : input.length > index
+          ? "incorrect"
+          : ""
+      }
+    >
+      {text}
+    </Character>
+  )
+);
